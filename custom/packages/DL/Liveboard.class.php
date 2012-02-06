001339d005ac795150923d203f912a8385663aaa
Pieter Colpaert <pieter@iRail.be>
2012-02-06 21:16:59 +0100

    Alpha De Lijn support for Liveboards

--- custom/packages/DL/Liveboard.class.php
+++ custom/packages/DL/Liveboard.class.php
@@ -13,10 +13,52 @@ include_once("custom/packages/iRailLiveboard.class.php");
 class DLLiveboard extends IRailLiveboard {
     
     public function call() {
-        $loc = $this->location;
+        $locid = $this->location;
+        //check if we have to resolve the location to an id ourself
+        if(!is_numeric($locid)){
+            $loc = DLStations::getStationFromName($this->location);
+            $locid= $loc["STOPIDENTIFIER"];
+        }
+        $this->locid = $locid;
         
+        $data = TDT::HttpRequest("http://reisinfo.delijn.be/realtime/halte/" . $locid);
+        $data = $this->parseDeLijn($data->data);
+        return $data;
     }
- 
+
+    private function parseDeLijn($data){
+        preg_match_all("/<tr class=\".*?\"(.*?)(?=<tr class=\".*?\")/smi",$data,$matches);
+        $results = array();
+        $results["station"] = DLStations::getStationFromId($this->locid);
+        $results["departures"] = array();
+        foreach($matches[1] as $row){
+            $dep = array();
+            preg_match("/<span id=\"form:haltebord:..?:lblPubliekNr\">(.*?)<\/span>/smi",$row,$match);
+            
+            if(isset($match[1])){
+                $dep["line"] = $match[1];
+            }
+            
+            preg_match("/<span id=\"form:haltebord:..?:lblBestemming\">(.*?)<\/span>/smi",$row,$match);
+            if(isset($match[1])){
+                $dep["direction"] = $match[1];
+            }
+            
+            preg_match("/<span id=\"form:haltebord:..?:verwacht\">(.*?)<\/span>/smi",$row,$match);
+            if(isset($match[1])){
+                $dep["time"] = $match[1];
+            }
+            
+            //DEV:$dep["dump"] = $row;
+            
+            $results["departures"][] = $dep;
+            
+            
+        }
+        return $results;
+        
+
+    }
 }
 
 ?>
--- custom/packages/DL/Stations.class.php
+++ custom/packages/DL/Stations.class.php
@@ -23,30 +23,34 @@ class DLStations extends iRailStations {
     }
     
     public function setParameter($key, $val) {
-        if($key== "municipal"){
+        if($key == "municipal"){
             $this->municipal = strtoupper($val);
         }
     }
 
 
     public function call() {
-        $arguments = array(":municipal" => $this->municipal);
+        $arguments = array(":municipal" => urldecode($this->municipal));
         $result = R::getAll("select * from delijn_stops where STOPPARENTMUNICIPAL like :municipal",$arguments);
         return $result;
-        
     }
     
     public static function getStationFromName($name, $lang = "en") {
+        $arguments = array(":name" => urldecode($name));
+        $result = R::getAll("select STOPIDENTIFIER from delijn_stops where STOPDESCRPTION like '%:name%'",$arguments);
+        if(isset($result[0])){
+            return $result[0]["STOPIDENTIFIER"];
+        }
+        return 0;
     }
-    
-    public static function getStationIdFromName($name, $lang = "en") {
-    }
-    
-    public static function getStationIdsFromName($names, $lang = "en") {
 
-    }
-    
-    public static function getStationsFromName($names, $lang = "en") {
+    public static function getStationFromId($id, $lang = "en") {
+        $arguments = array(":id" => $id);
+        $result = R::getAll("select STOPDESCRPTION from delijn_stops where STOPIDENTIFIER = :id",$arguments);
+        if(isset($result[0])){
+            return $result[0]["STOPDESCRPTION"];
+        }
+        return "";
     }
 }
 
