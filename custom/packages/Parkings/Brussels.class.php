154fadd09d16eea5db730a59833e6ddc0efd6fe1
Pieter Colpaert <pieter@iRail.be>
2012-03-16 12:39:28 +0100

    Added real-time parking information of Brussels to iRail API

---
+++ custom/packages/Parkings/Brussels.class.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once("custom/packages/DL/tools.php");
+class ParkingsBrussels extends AResource{
+     public static function getRequiredParameters(){
+	  return array(); 
+     }
+
+     public static function getParameters(){
+	  return array();
+     }
+
+     public static function getDoc(){
+	  return "This resource contains real-time information on parkings in Brussels thanks to irisnet.be";
+	  
+     }
+     
+     public function setParameter($key,$val){
+     }
+     
+
+     public function call(){
+         $data = TDT::HttpRequest("http://www.mobielbrussel.irisnet.be/static/categories_files/parkings.json");
+         $object = json_decode($data->data);
+         foreach($object->features as &$element){
+             $coords = tools::LambertToWGS84($element->geometry->coordinates[0],$element->geometry->coordinates[1]);
+             $element->latitude = $coords[0];
+             $element->longitude = $coords[1];
+             $element->name_fr = $element->properties->nom;
+             $element->name_nl = $element->properties->naam;
+             $element->city_nl = $element->properties->stad;
+             $element->city_fr = $element->properties->ville;
+             $element->zipcode = (int)$element->properties->post_code;
+             $element->address_nl =$element->properties->adres;
+             $element->address_fr =$element->properties->adresse;
+             $element->free_places = $element->properties->free_places;
+             $element->total_places = $element->properties->total_places;
+             unset($element->properties);
+             unset($element->geometry);
+         }
+         return $object->features;
+         
+     }
+
+     
+}
+?>
+
---
+++ custom/packages/Parkings/resources.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * @copyright (C) 2011 by iRail vzw/asbl
+ * @license AGPLv3
+ * @author Pieter Colpaert   <pieter@iRail.be>
+ */
+
+class Parkings{
+     /**
+      * All available methods in the module TDTInfo
+      */
+     public static $resources = array (
+	  "Brussels"
+	  );
+}
+?>
+
